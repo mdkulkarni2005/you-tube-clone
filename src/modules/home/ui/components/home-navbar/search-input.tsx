9cb166225b3fedce97dfec63229223c0751d1f15
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-05-13 17:59:22 +0530

    Refactor utility functions and UI components

---
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -0,0 +1,9 @@
+export const SearchInput = () => {
+    return (
+        <form className="flex w-full max-w-[600px]" action="">
+            <div className="relative w-full">
+                <input type="text" placeholder="search" className="" />
+            </div>
+        </form>
+    )
+}
